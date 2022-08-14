c65cee185ad7b0150cacfa4de99843b5a341b2aa
xeonsim <byeonggeonsim@gmail.com>
2022-08-14 16:34:11 +0900

    error fix

--- api-server.js
+++ api-server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const { CONTRACT_ABI, CONTRACT_ADDRESS } = require("./contract/config.js");
+const { CONTRACT_ABI, CONTRACT_ADDRESS } = require("./contract/artKonnection.js");
 const Caver = require("caver-js");
 const app = express();
 const router = express.Router();
